68cf0c767e86ca2158cd185fe6365d71695882f8
Joseph Mensah <josephtettehmensah37@gmail.com>
2026-07-24 01:16:12 +0000

    Delete old folders

--- routes/notificationRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { notification } = require('../controllers/notificationController');
-
-//Route to send notification
-router.post('/', notification);
-
-module.exports = router;
